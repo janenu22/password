d808f6ae60240abe34f3c3593700932b1870b9db
Rea Yan <janenu22@gmail.com>
2019-06-13 20:43:21 +0800

    update

--- password.py
+++ password.py
@@ -1,9 +1,13 @@
-turn = 1
-while turn <4:	
+turn = 3
+while turn >0:	
+	turn = turn-1
 	password = input('請輸入密碼: ')
 	if password == 'a12345':
 		print('登入成功!')
 		break
 	else:
-		print('密碼錯誤，還有', 3-turn, '次機會。 ')
-		turn = turn + 1
+		if turn >0:
+			print('密碼錯誤，還有', turn, '次機會。 ')
+		else:
+			print('三次輸入錯誤，帳號被鎖。')
+		
